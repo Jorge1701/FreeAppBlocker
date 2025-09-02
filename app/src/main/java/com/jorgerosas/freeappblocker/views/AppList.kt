2ec5d71bb8cef052e441373f0b4b3704c7d7bb6d
Jorge Rosas <rosasjorge1701@gmail.com>
2025-09-01 22:35:30 -0300

    Added main activity (#10)

---
+++ app/src/main/java/com/jorgerosas/freeappblocker/views/AppList.kt
@@ -0,0 +1,12 @@
+package com.jorgerosas.freeappblocker.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jorgerosas.freeappblocker.R
+
+class AppList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_list)
+    }
+}
